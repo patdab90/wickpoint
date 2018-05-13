f4f3665231a54663d0090d73222094665df726cf
Patryk Dąbrowski <pdabrowski@egnyte.com>
2018-05-13 22:39:41 +0200

    number of authors

--- commitsnumber.sh
+++ commitsnumber.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 files=$(git ls-files | grep "\.java" | grep -v "target/generated-sources" | grep -v "test")
 for file in $files; do
+	authors=$(git log --pretty=format:"%an%x09" "$file" | sort | uniq | grep -v "Jenkins" | wc -l)
 	changes=$(git log --oneline -- $file | wc -l)
-	echo "$file, $changes"
+	last_modified=$(git log -1 --format="%ai" -- "$file")
+	echo "$file, $last_modified, $authors, $changes"
 done
